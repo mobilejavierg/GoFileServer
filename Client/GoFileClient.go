cb1c4d46cb12908ef116600202d11447818c0d37
mobilejavierg <mobile.javierg@gmail.com>
2020-07-16 17:48:06 -0300

    modified:   GoFileClient.go
    	modified:   ../server/GoFileServer.go

--- Client/GoFileClient.go
+++ Client/GoFileClient.go
@@ -1,60 +1,297 @@
 package main
 
 import (
-    "bytes"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "mime/multipart"
-    "net/http"
-    "os"
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	customloggerC "github.com/mobilejavierg/logs"
+	"github.com/mobilejavierg/ofiles"
+	//    "github.com/fsnotify/fsnotify"
 )
 
-func postFile(filename string, targetURL string) error {
-    bodyBuf := &bytes.Buffer{}
-    bodyWriter := multipart.NewWriter(bodyBuf)
+//LLoggerC log
+var LLoggerC *customloggerC.Logger
 
-    // this step is very important
-    fileWriter, err := bodyWriter.CreateFormFile("uploadfile", filename)
-    if err != nil {
-        fmt.Println("error writing to buffer")
-        return err
-    }
+func existeArchivo(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if os.IsNotExist(err) {
+		return false, nil
+	}
 
-    // open file handle
-    fh, err := os.Open(filename)
-    if err != nil {
-        fmt.Println("error opening file")
-        return err
-    }
-    defer fh.Close()
+	/*quise poner todo esto en una linea, pero no lo logre :( */
+	existe := true
+	if err != nil {
+		LLoggerC.Println("existeArchivo " + name + " - " + err.Error())
+		existe = false
+	}
 
-    //iocopy
-    _, err = io.Copy(fileWriter, fh)
-    if err != nil {
-        return err
-    }
-
-    contentType := bodyWriter.FormDataContentType()
-    bodyWriter.Close()
-
-    resp, err := http.Post(targetURL, contentType, bodyBuf)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-    respbody, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return err
-    }
-    fmt.Println(resp.Status)
-    fmt.Println(string(respbody))
-    return nil
+	return existe, err
 }
 
-// sample usage
+func confirmarArchivo(_directorio string, _archivoOK string) {
+
+	archivoNuevo := ""
+
+	if _directorio != "" {
+
+		i := strings.Index(_archivoOK, strings.Replace(_directorio, "\\\\", "\\", -1))
+		if i > -1 {
+			archivoNuevo = _archivoOK + ".ok"
+		} else {
+			archivoNuevo = filepath.Join(_directorio, _archivoOK+".ok")
+		}
+	} else {
+		archivoNuevo = _archivoOK + ".ok"
+	}
+
+	newFile, err := os.Create(archivoNuevo)
+	if err != nil {
+		LLoggerC.Println("creando archivo Ok " + archivoNuevo + " - " + err.Error())
+		fmt.Println("creando archivo Ok " + archivoNuevo + " - " + err.Error())
+	}
+	newFile.Close()
+}
+
+func procesarArchivos(_directorio string, _url string, _extension string, _destino string, _minutespan int64) {
+
+	files, err := ioutil.ReadDir(_directorio)
+
+	if err != nil {
+		LLoggerC.Println("error accediendo a " + _directorio + " - " + err.Error())
+		fmt.Println("error accediendo a " + _directorio + " - " + err.Error())
+	}
+
+	// utc life
+	timeloc, _ := time.LoadLocation("UTC")
+	nowTime := time.Now().In(timeloc)
+
+	//	LLoggerC.Println("error accediendo a " + _directorio + " - " + err.Error())
+
+	for _, f := range files {
+		if !f.IsDir() && strings.HasSuffix(f.Name(), _extension) {
+
+			filename := f.Name()
+
+			existe, err := existeArchivo(filepath.Join(_directorio, filename+".ok"))
+
+			if err != nil {
+				fmt.Println("Error:" + filepath.Join(_directorio, filename+".ok") + " " + err.Error())
+				//				LLoggerC.Println("Error:" + filepath.Join(_directorio, filename+".ok") + " " + err.Error())
+			}
+
+			if existe {
+				fmt.Println("archivo " + filepath.Join(_directorio, filename) + " ya enviado previamente")
+				LLoggerC.Println("archivo " + filepath.Join(_directorio, filename) + " ya enviado previamente")
+			} else {
+
+				filecurrTime := f.ModTime()
+				diff := int64(nowTime.Sub(filecurrTime).Minutes())
+
+				if (_minutespan == 0) || (_minutespan > 0 && (diff > _minutespan)) {
+					fmt.Println(filepath.Join(_directorio, filename))
+					if _minutespan > 0 {
+						fmt.Println(" " + strconv.FormatInt(diff, 10) + " minutos de vida")
+					}
+					LLoggerC.Println("enviando " + filename)
+					postFile(_directorio, filename, _url, _destino, "")
+				}
+			}
+		}
+	}
+}
+
+func postFile(directorio string, _filename string, targetURL string, destino string, archivoFullpath string) error {
+
+	var _archivoProcesar string
+
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	if archivoFullpath != "" {
+		_filename = filepath.Base(archivoFullpath)
+	}
+	// this step is very important
+
+	fileWriter, err := bodyWriter.CreateFormFile("uploadfile", _filename)
+	if err != nil {
+		LLoggerC.Println("error writing to buffer -- uploadfile" + _filename + " - " + err.Error())
+		fmt.Println("error writing to buffer " + err.Error())
+		os.Exit(1)
+	}
+
+	if archivoFullpath == "" {
+		_archivoProcesar = filepath.Join(directorio, _filename)
+	} else {
+		_archivoProcesar = archivoFullpath
+	}
+
+	// open file handle
+
+	fmt.Println("open file handle")
+
+	fh, err := os.Open(_archivoProcesar)
+	if err != nil {
+		LLoggerC.Println("error opening file -- open file handle" + _archivoProcesar + " - " + err.Error())
+		fmt.Println("error opening file" + _archivoProcesar + " - " + err.Error())
+		os.Exit(1)
+	}
+	defer fh.Close()
+
+	//gethash
+	_hash, _ := ofiles.Hashfilemd5(_archivoProcesar)
+
+	//iocopy
+	fmt.Println("iocopy")
+
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		fmt.Println("error io.copy " + " - " + err.Error())
+		LLoggerC.Println("error io.copy " + " - " + err.Error())
+		os.Exit(1)
+	}
+
+	bodyWriter.WriteField("hash", _hash)
+	bodyWriter.WriteField("destino", destino)
+
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	resp, err := http.Post(targetURL, contentType, bodyBuf)
+
+	if err != nil {
+		fmt.Println("error post " + targetURL + " - " + err.Error())
+		LLoggerC.Println("error post " + targetURL + " - " + err.Error())
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+	respbody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("error ioutil.ReadAll " + " - " + err.Error())
+		LLoggerC.Println("error ioutil.ReadAll " + " - " + err.Error())
+		os.Exit(1)
+	}
+
+	if string(respbody) == "OK" {
+		LLoggerC.Println("enviado ok \r")
+		confirmarArchivo(directorio, _archivoProcesar)
+	}
+
+	return nil
+	//fmt.Println(resp.Status)
+	//fmt.Println(string(bodyBuf.String()))
+	//fmt.Println(string(contentType))
+
+}
+
 func main() {
-    targeturl := "http://localhost:8080/upload"
-    filename := "arduino.rar"
-    postFile(filename, targeturl)
+
+	var _Servidor string
+	var _puerto string
+	var _Directorio string
+	var _archivo string
+	var _Extensionarchivo string
+	var _destino string
+	var _lifeSpan int64
+
+	flag.StringVar(&_Servidor, "ip", "", "direccion servidor (192.168.2.90)") // (http://192.168.2.90:1678/upload)")
+	flag.StringVar(&_puerto, "port", "1678", "puerto del servidor (port=9999)")
+
+	flag.StringVar(&_Directorio, "path", "", "directorio origen (path=\"c:\\archivos ret\\\\\" doble barra al final)")
+	flag.StringVar(&_archivo, "archivo", "", "fullpath + archivo archivo=c:\\carpeta\\archivo.ext, si envia este paramtro se ingora [PATH] y [EXT]")
+
+	flag.StringVar(&_Extensionarchivo, "ext", "", "extension del archivo a enviar (ext=ret)")
+	flag.StringVar(&_destino, "dest", "", "destino en servidor (ext=\\678\\20276862465\\)")
+	flag.Int64Var(&_lifeSpan, "span", 0, "_lifeSpam, tiempo de vida para copiar, por defecto 0 (deshabilitado)")
+
+	flag.Parse()
+
+	if _Directorio == "" && _archivo == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if _Directorio != "" && (_Servidor == "" || _Extensionarchivo == "" || _puerto == "") {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if _archivo != "" && (_Servidor == "" || _puerto == "") {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	if !strings.HasSuffix(_Directorio, "\\") {
+		_Directorio = strings.TrimSpace(_Directorio) + "\\"
+	}
+
+	LLoggerC = customloggerC.GetInstance("gofileclient", "client ")
+
+	fmt.Println(_Directorio)
+	fmt.Println("Alpha 2000 Soluciones Informaticas")
+	fmt.Println("Servidor: " + _Servidor + ":" + _puerto)
+
+	_urlServidor := "http://" + _Servidor + ":" + _puerto + "/upload"
+
+	LLoggerC.Println(" Iniciando copia desde " + _Directorio + " al Servidor: " + _Servidor + ":" + _puerto)
+
+	if _archivo == "" {
+		procesarArchivos(_Directorio, _urlServidor, _Extensionarchivo, _destino, _lifeSpan)
+	} else {
+		fmt.Println(_archivo)
+		postFile("", "", _urlServidor, _destino, _archivo)
+	}
+
 }
+
+/*
+
+var _hash string
+
+watcher, err := fsnotify.NewWatcher()
+if err != nil {
+    fmt.Println(err)
+}
+defer watcher.Close()
+
+done := make(chan bool)
+go func() {
+    for {
+        select {
+        case event := <-watcher.Events:
+
+            fmt.Println("event:", event)
+            if event.Op&fsnotify.Write == fsnotify.Write {
+
+//                    _hash, _ = ofiles.Hashfilemd5(strings.Replace(event.Name,'d','d',-1))
+
+                fmt.Println("modified file:", event.Name, " hash: " , _hash)
+            }
+            if event.Op&fsnotify.Create == fsnotify.Create {
+                fmt.Println("created file:", event.Name)
+            }
+            _hash, _ = ofiles.Hashfilemd5(event.Name)
+            fmt.Println(_hash)
+
+        case err := <-watcher.Errors:
+            fmt.Println("error:", err)
+        }
+    }
+}()
+
+err = watcher.Add("C:/_cryptoAPI/")
+if err != nil {
+    fmt.Println(err)
+}
+<-done
+*/
--- server/GoFileServer.go
+++ server/GoFileServer.go
@@ -1,51 +1,148 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"time"
 	"crypto/md5"
+	"encoding/json"
+	"fmt"
 	"io"
-	"strconv"
+	"net/http"
 	"os"
-	"html/template"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	customlogger "github.com/mobilejavierg/logs"
+	"github.com/mobilejavierg/ofiles"
 )
 
-func main(){
-	http.HandleFunc("/upload", upload)
-	http.ListenAndServe(":8080",nil)
+//Configuration configuracion
+type Configuration struct {
+	Puerto         string
+	DirectorioRaiz string
 }
 
+var _configuration Configuration
+
+//LLogger log
+var LLogger *customlogger.Logger
+
+func main() {
+
+	LLogger = customlogger.GetInstance("alphaFileServer", "main ")
+	LLogger.Println("#### Inicia servicio GoFileServer ####\r")
+
+	//lee archivo de configuracion
+	getConfiguracion()
+
+	//si no existe el directotio raiz
+	if _, err := os.Stat(_configuration.DirectorioRaiz); os.IsNotExist(err) {
+		// creo el DirectorioRaiz
+		os.MkdirAll(_configuration.DirectorioRaiz, os.ModePerm)
+	}
+
+	http.HandleFunc("/upload", upload)
+	errHTTP := http.ListenAndServe(":"+_configuration.Puerto, nil)
+
+	if errHTTP != nil {
+		LLogger.Println(errHTTP)
+		fmt.Println(errHTTP)
+		return
+	}
+
+}
+
+func getConfiguracion() {
+
+	//////////////////////////////////////////////////////////////////////
+	// leer archivo configuracion
+	LLogger.Println("levantando configuracion buscando alphafileserver.json")
+
+	//filename is the path to the json config file
+	file, err := os.Open("alphafileserver.json")
+
+	if err != nil {
+		LLogger.Println("ERROR open gofileserver.json " + err.Error())
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&_configuration)
+	if err != nil {
+		LLogger.Println("ERROR decode json " + err.Error())
+		fmt.Println(err)
+	}
+
+	fmt.Println("escuchado: " + _configuration.Puerto)
+	fmt.Println("directorio Raiz: " + _configuration.DirectorioRaiz)
+
+	// fin archivo de configuracion
+	//////////////////////////////////////////////////////////////////////
+}
 
 // upload logic
 func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
+
+	var _destino string
+	//	logger := customlogger.GetInstance("goFileServer", "Main UpLoad ")
+	LLogger.Println("\rconneccion desde: " + r.RemoteAddr + " metodo: " + r.Method + "\r")
+
 	if r.Method == "GET" {
 		crutime := time.Now().Unix()
 		h := md5.New()
 		io.WriteString(h, strconv.FormatInt(crutime, 10))
 		token := fmt.Sprintf("%x", h.Sum(nil))
+		fmt.Fprintf(w, token)
 
-		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
+		/*
+			t, _ := template.ParseFiles("upload.gtpl")
+			t.Execute(w, token)
+			t.WriteString(token)
+		*/
 	} else {
+
 		r.ParseMultipartForm(32 << 20)
 		file, handler, err := r.FormFile("uploadfile")
 		if err != nil {
+			LLogger.Println(err.Error() + "\r")
 			fmt.Println(err)
 			return
 		}
 		defer file.Close()
 
-		fmt.Println("archivo recibido: " + handler.Filename + " tamaño: " + strconv.FormatInt((handler.Size/1024), 10) + " KB")
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		_hashorigen := r.FormValue("hash")
+		_destino = r.FormValue("destino")
+		_destino = filepath.Join(_configuration.DirectorioRaiz, _destino, "\\")
+
+		//si no existe el directotio raiz
+		if _, err := os.Stat(_destino); os.IsNotExist(err) {
+			// creo el DirectorioRaiz
+			os.MkdirAll(_destino, os.ModePerm)
+		}
+
+		LLogger.Println("archivo recibido: " + handler.Filename + " tamaño: " + strconv.FormatInt((handler.Size/1024), 10) + " KB  hash: " + r.FormValue("hash") + "\r")
+
+		f, err := os.OpenFile(_destino+"\\"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		LLogger.Println("generando " + _destino + "\\" + handler.Filename)
+
 		if err != nil {
+			LLogger.Println(err.Error() + "\r")
 			fmt.Println(err)
 			return
 		}
 		defer f.Close()
 		io.Copy(f, file)
-		fmt.Println("guardado en ")
+
+		//gethash
+		_hashDestino, _ := ofiles.Hashfilemd5(filepath.Join(_destino + "\\" + handler.Filename))
+
+		if _hashorigen != _hashDestino {
+			LLogger.Println(w, "%v", "Error Hash: "+_hashorigen+" -> "+_hashDestino)
+			fmt.Fprintf(w, "%v", "Error Hash: "+_hashorigen+" -> "+_hashDestino)
+		} else {
+			fmt.Fprintf(w, "%v", "OK")
+			LLogger.Println("OK " + f.Name() + " hash: " + _hashorigen + "\r")
+		}
+
 	}
 }
